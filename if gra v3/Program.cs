ca2212cb064bb01d046eb2553ab4fe20bcac40fe
Igor <igorkris719@gmail.com>
2024-03-02 19:26:44 +0100

    dodalem nowe rzeczy

--- if gra v3/Program.cs
+++ if gra v3/Program.cs
@@ -16,6 +16,7 @@ while (życie >= 0) {
         życie = 75;
 
     } else {
+        Console.WriteLine();
         Console.WriteLine("ok miałeś szczęście...");
     }
     Console.WriteLine("czy trafiono cię karabinem?");
